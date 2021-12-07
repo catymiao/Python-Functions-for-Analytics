db8805fd90751c8f11bc13cc44985ffa40cdfbff
catymiao <33770231+catymiao@users.noreply.github.com>
2021-12-07 16:14:46 -0600

    Update Calculate Age from DOB.py

--- Calculate Age from DOB.py
+++ Calculate Age from DOB.py
@@ -1,7 +1,7 @@
 from datetime import date
  
 def calculateAge(birthDate):
-    today = date.today()
+    today = date.today() ## date(2021,12,2)
     age = today.year - birthDate.year -((today.month, today.day) <
          (birthDate.month, birthDate.day))
  
